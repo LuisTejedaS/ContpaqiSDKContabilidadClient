02d1479012ce3381e02396b4b68c0316880f8c6d
Luis Tejeda <luis.tejeda@contpaqi.com>
2019-01-03 14:36:07 -0600

    Agregar lista de guid para relacionar

--- AccountingClient/AccountingClient.Schema/JournalEntryMovement.cs
+++ AccountingClient/AccountingClient.Schema/JournalEntryMovement.cs
@@ -4,26 +4,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace AccountingClient.Schema 
+namespace AccountingClient.Schema
 {
     public class JournalEntryMovement
     {
-
-    
         public int Number { get; set; }
-
         public string Account { get; set; }
-
         public JournalEntryMovementType JournalEntryMovementType { get; set; }
-
         public int JournalEntryMovementDaily { get; set; }
         public string Concept { get; set; }
-
+        public string Reference { get; set; }
         public decimal Amount { get; set; }
         public decimal AmountFC { get; set; }
-
         public Guid Guid { get; set; }
         public string UUIDReferences { get; set; }
+        public List<Guid> UUIDReferenciesGuid { get; set; }
 
 
     }
