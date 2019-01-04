756b24576e7028eff84b3b9349faeec9efa38c54
Luis Tejeda <luis.tejeda@contpaqi.com>
2019-01-04 10:50:55 -0600

    Agregar income

--- AccountingClient/AccountingClient.Schema/Income.cs
+++ AccountingClient/AccountingClient.Schema/Income.cs
@@ -4,13 +4,24 @@ using System.Text;
 
 namespace AccountingClient.Schema
 {
-    public class Supplier
+    public class Income
     {
-        public string Name { get; set; }
-        public string RFC { get; set; }
-        public string Account { get; set; }
-        public string ComplementaryAccount { get; set; }
-        public string ExpenseAccount { get; set; }
-        public string ComplementaryExpenseAccount { get; set; }
+        public string BankAccount { get; set; }
+        public string Date { get; set; }
+        public string DocumentType { get; set; }
+        public string Number { get; set; }
+        public string PayerCode { get; set; }
+        public string PayerName { get; set; }
+        public string Total { get; set; }
+        public string Reference { get; set; }
+        public string Concept { get; set; }
+        public string PaymentMethod { get; set; }
+        public string BankSource { get; set; }
+        public string BankAccountSource { get; set; }
+        public string Currency { get; set; }
+        public string ExchangeRate { get; set; }
+        public string Guid { get; set; }
+        public List<Guid> UUIDReferenciesGuid { get; set; }
+
     }
 }
